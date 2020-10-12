8f28e0989943713eabb0f7b575b48c1f54258896
Sake <pan.ziqiang@holytiny.com>
2020-10-12 09:47:26 +0800

    test(remove): add remove property of dictionary

--- test/index.test.ts
+++ test/index.test.ts
@@ -61,13 +61,6 @@ describe('yprofile profile', () => {
 describe('yprofile op', () => {
   // test
   // .stdout()
-  // .do(() => cmd.run([]))
-  // .it('runs hello', ctx => {
-  //   expect(ctx.stdout).to.contain('hello world')
-  // })
-
-  // test
-  // .stdout()
   // .do(() => cmd.run(['--name', 'jeff']))
   // .it('runs hello --name jeff', ctx => {
   //   expect(ctx.stdout).to.contain('hello jeff')
